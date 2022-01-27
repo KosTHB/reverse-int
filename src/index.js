540e747a303335ee2b69e297478e3974ce4a34dd
Konstantin Nepovinnov <nekonstantin@yandex.ru>
2022-01-27 19:52:17 +0400

    feat: init and end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n)
+  let arr = n.toString(10).replace(/\D/g, '0').split('').map(Number);
+  n = arr.reverse();
+  var res = (Number(n.join().replace(/,/g, '')))
+  return res;
 }
